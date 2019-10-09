f7e5f8ef351465b9f7f8e31c437be373e080d908
zhangzhen1234 <1483003064@qq.com>
2019-10-09 15:24:52 +0800

    增加左边栅栏和实现路由插座

--- src/container/Container.vue
+++ src/container/Container.vue
@@ -3,50 +3,7 @@
     <el-container>
       <el-aside class="app-side app-side-left"
                 :class="isCollapse ? 'app-side-collapsed' : 'app-side-expanded'">
-        <div class="app-side-logo">
-          <img src="@/assets/logo.png"
-               :width="isCollapse ? '60' : '60'"
-               height="60" />
-        </div>
-        <div>
-          <!-- 我是样例菜单 -->
-          <el-menu default-active="1-4-1"
-                   class="el-menu-vertical-demo"
-                   @open="handleOpen"
-                   :collapse="isCollapse">
-            <el-submenu index="1">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span slot="title">导航一</span>
-              </template>
-              <el-menu-item-group>
-                <span slot="title">分组一</span>
-                <el-menu-item index="1-1">选项1</el-menu-item>
-                <el-menu-item index="1-2">选项2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="分组2">
-                <el-menu-item index="1-3">选项3</el-menu-item>
-              </el-menu-item-group>
-              <el-submenu index="1-4">
-                <span slot="title">选项4</span>
-                <el-menu-item index="1-4-1">选项1</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="2">
-              <i class="el-icon-menu"></i>
-              <span slot="title">导航二</span>
-            </el-menu-item>
-            <el-menu-item index="3"
-                          disabled>
-              <i class="el-icon-document"></i>
-              <span slot="title">导航三</span>
-            </el-menu-item>
-            <el-menu-item index="4">
-              <i class="el-icon-setting"></i>
-              <span slot="title">导航四</span>
-            </el-menu-item>
-          </el-menu>
-        </div>
+        <Sidebar :collapse="isCollapse" :routes="$router.options.routes[1].children"/>
       </el-aside>
 
       <el-container>
@@ -57,29 +14,13 @@
             <i v-show="isCollapse" class="el-icon-d-arrow-right"></i>
           </div>
           <!-- 我是样例菜单 -->
-          <el-menu default-active="1"
+          <el-menu default-active="/"
+                   router
                    class="el-menu-demo tab-page"
                    mode="horizontal"
                    @select="handleSelect"
                    active-text-color="#409EFF">
-            <el-menu-item index="1">处理中心</el-menu-item>
-            <el-submenu index="2">
-              <template slot="title">我的工作台</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-              <el-menu-item index="2-3">选项3</el-menu-item>
-              <el-submenu index="2-4">
-                <template slot="title">选项4</template>
-                <el-menu-item index="2-4-1">选项1</el-menu-item>
-                <el-menu-item index="2-4-2">选项2</el-menu-item>
-                <el-menu-item index="2-4-3">选项3</el-menu-item>
-              </el-submenu>
-            </el-submenu>
-            <el-menu-item index="3"
-                          disabled>消息中心</el-menu-item>
-            <el-menu-item index="4">
-              <a href="#">订单管理</a>
-            </el-menu-item>
+            <el-menu-item index="/">首页</el-menu-item>
           </el-menu>
 
           <div class="app-header-userinfo">
@@ -110,8 +51,12 @@
 </template>
 
 <script>
-    export default {
-        name: 'Container',
+  import Sidebar from '../components/Sidebar'
+  export default {
+        name: 'Dashboard',
+        components: {
+            Sidebar
+        },
         data() {
             return {
                 username: '',
@@ -151,4 +96,3 @@
 
 <style>
 </style>
-
